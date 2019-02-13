23a4090a38e67777caae253197cad99b961e440e
rebeccapeltz <rebeccapeltz@gmail.com>
2019-02-13 14:03:44 -0800

    add code and build

--- src/router.js
+++ src/router.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 // TODO: Import the Survey component
+import Survey from '@/views/Survey'
 // TODO: Import the Secret component
+import Secret from '@/views/Secret'
 
 Vue.use(Router)
 
@@ -10,9 +12,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
+      name: 'home',
       component: Home
+    },
+    {
+      path: '/survey',
+      name: 'survey',
+      component: Survey
+    },
+    {
+      path: '/secret',
+      name: 'secret',
+      component: Secret
     }
+
     // Add the Survey route definition
     // Add the Secret route definition
   ]
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,62 @@
 <template>
   <div class="home">
-    <div class="form-container"><!-- TODO: Modulate display of the .form-container using v-show and the showForm variable. -->
+    <div class="form-container" v-show="showForm">
+      <!-- TODO: Modulate display of the .form-container using v-show and the showForm variable. -->
       <h1>Join the Web Developers Club!</h1>
       <p>Sign up to access our special, secret page. Just create an account and answer a brief survey.</p>
 
       <!-- TODO: Add an HTML element to display an error message for when the user submits invalid information. Use v-show to show/hide this message based on the validity of the form data. -->
+      <p
+        class="error"
+        v-show="showError"
+      >Please check the information you have entered. Be sure to fill in all the fields.</p>
 
-      <form><!-- TODO: Add the proper v-on directive to let the validateForm method handle this form when it is submitted -->
-
+      <form v-on:submit.prevent="validateForm">
+        <!-- TODO: Add the proper v-on directive to let the validateForm method handle this form when it is submitted -->
         <!-- TODO: Add labels for each form field in this form. -->
+        <p>
+          <label for="username">
+            Username
+            <input type="text" id="username" v-model="username">
+          </label>
+        </p>
 
         <!-- TODO: Add a username field. -->
+        <p>
+          <label for="email">
+            Email
+            <input type="email" id="email" v-model="email">
+          </label>
+        </p>
 
         <!-- TODO: Add an email field. -->
+        <label for="password">
+          Password
+          <input type="password" id="password" v-model="password">
+        </label>
 
         <!-- TODO: Add a password field. -->
-
-        <!-- TODO: Add a passwordVerify field. -->
-
-
-        <p><input type="submit" value="Submit"></p>
+        <p>
+          <label for="passwordVerify">
+            Verify Password
+            <input type="password" id="passwordVerify" v-model="passwordVerify">
+          </label>
+        </p>
+        <p>
+          <!-- TODO: Add a passwordVerify field. -->
+        </p>
+        <p>
+          <input type="submit" value="Submit">
+        </p>
       </form>
     </div>
-    <div class="success-message"><!-- TODO: Modulate display of the .success-message using v-show and the showForm variable. -->
+    <div class="success-message" v-show="!showForm">
+      <!-- TODO: Modulate display of the .success-message using v-show and the showForm variable. -->
       <h1>Thank you for signing up!</h1>
-      <p>Please take our new member survey. Click here</p><!-- TODO: Link "Click here" to the survey page. -->
+      <p>
+        Please take our new member survey. <router-link to="/survey">Click here</router-link>
+      </p>
+      <!-- TODO: Link "Click here" to the survey page. -->
     </div>
   </div>
 </template>
@@ -44,6 +76,16 @@ export default {
   },
   methods: {
     validateForm: function () {
+      if ((this.username !== '') &&
+        (this.email !== '') &&
+        //check for empty password
+        (this.password === this.passwordVerify)
+      ) {
+        this.showForm = false;
+      } else {
+        this.showError = true;
+      }
+
       // Validate the form by checking the following values:
       // username must not be blank
       // email must not be blank
@@ -64,7 +106,8 @@ export default {
   padding: 1rem;
   color: #aa0000;
 }
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
